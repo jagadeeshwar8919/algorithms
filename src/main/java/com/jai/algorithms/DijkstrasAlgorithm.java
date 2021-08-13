3f10d4a719da91da353c3cf0e0e73a7cf6073c33
C5307501 <jagadeeswar.reddy@sap.com>
2021-08-13 11:32:56 +0530

    Accepting values from user

--- src/main/java/com/jai/algorithms/DijkstrasAlgorithm.java
+++ src/main/java/com/jai/algorithms/DijkstrasAlgorithm.java
@@ -1,20 +1,34 @@
 package com.jai.algorithms;
 
+import java.util.Scanner;
+
 public class DijkstrasAlgorithm {
 
 public static int size=9;
 
     static public void duplicatemain(){
+        Scanner scan=new Scanner(System.in);
+        int v=scan.nextInt();
+        int e=scan.nextInt();
+        int graph[][]=new int[v][v];
+        for(int i=0;i<e;i++){
+            int v1=scan.nextInt();
+            int v2=scan.nextInt();
+            int weight=scan.nextInt();
+            graph[v1][v2]=weight;
+            graph[v2][v1]=weight;
+        }
 
-        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+//        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+//                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+//                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+//                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+//                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+//                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+//                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+//                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+//                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
 
 
         dijkstra(graph, 0);
@@ -42,7 +56,7 @@ public static int size=9;
             }
         }
 
-       for(int i=1;i<v;i++){
+       for(int i=0;i<v;i++){
            System.out.println(i+"  "+distance[i]);
        }
     }
